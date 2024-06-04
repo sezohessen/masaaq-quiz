807c04a0c530cc7bbdc24dee3f2b33f6ae3963dc
sezohessen <sezohessen2000@gmail.com>
2024-06-04 14:45:06 +0300

    👌 IMPROVE: make middleware for administrator

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Administrator;
 use App\Http\Middleware\ClientOwner;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain;
@@ -72,5 +73,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'client_owner' => ClientOwner::class,
+        'administrator' => Administrator::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth','administrator'])
+    ->prefix('dashboard')
+    ->name('dashboard.')
+    ->group(function () {
+        Route::get('/', function () {
+            return view('dashboard');
+        })->name('index');
+    });
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

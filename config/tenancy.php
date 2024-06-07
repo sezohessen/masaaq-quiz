5e74addaedebf829cb81ca7f319ba882e54af2b9
sezohessen <sezohessen2000@gmail.com>
2024-06-08 01:39:56 +0300

    👌 IMPROVE: change central domain to localhost to accepted by google calendar redirect URLs

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,7 +18,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'quizzes',
+        'localhost',
     ],
 
     /**

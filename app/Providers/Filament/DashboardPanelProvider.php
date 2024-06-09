ba73d2cd5bc4b09e93407f9f63145124988d8803
sezohessen <sezohessen2000@gmail.com>
2024-06-09 07:29:29 +0300

    👌 IMPROVE: Panel configuration

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -28,8 +28,10 @@ class DashboardPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Green,
             ])
+            ->font('Poppins')
+            ->favicon(asset('images/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ global_asset('images/logo.png') }}" alt="Logo" class="h-10 inline-block">
+<span>{{ env('APP_NAME') }}</span>

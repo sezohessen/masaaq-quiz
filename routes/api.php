3144b146fc448116a0e0234790deeedf4e5fc0ed
sezohessen <sezohessen2000@gmail.com>
2024-06-10 08:17:08 +0300

    👌 IMPROVE: initialize tenancy by request data of X-Tenant

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByRequestData;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware([
+    'api',
+    InitializeTenancyByRequestData::class
+])->name('api.')
+->prefix('/v1')
+->group(function () {
+    Route::get('test',function(){
+        return dd(tenancy()->tenant->id);
+    });
 });
